af838519105cbb841a03bdd6036f691168b81807
Tobey Unruh <tobeyunruh@gmail.com>
2017-03-22 06:39:08 -0400

    Initial commit

---
+++ Zawoo-Homework/src/Zawoo/Lab5.java
@@ -0,0 +1,17 @@
+package Zawoo;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class Lab5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void RandomString()
+	{
+		
+	}
+}
