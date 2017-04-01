6b6b8bc5c0c99bf587f76c328ca598d1a1870c89
Tobey Unruh <tobeyunruh@gmail.com>
2017-04-01 14:46:48 -0400

    Added getHourlyRate, getSalary, and getGrossIncome functions

--- Zawoo-Homework/src/Zawoo/PROGRAM2.java
+++ Zawoo-Homework/src/Zawoo/PROGRAM2.java
@@ -13,9 +13,10 @@ public class PROGRAM2
 		String[] employeeNames;
 		String[] employeeIds;
 		Boolean[] hourlyEmployee;
+		Boolean[] localResidents;
 		float[] hourlyWorked;
 		float[] hourlyRate;
-		float[] salaries;
+		float[] salaryRate;
 		float[] grossIncome;
 		float[] netIncome;
 		
@@ -25,8 +26,19 @@ public class PROGRAM2
 		// get number of employees to process
 		NumOfEmployees();
 		
+		// allocate arrays
+		employeeNames = new String[numOfEmployees];
+		employeeIds = new String[numOfEmployees];
+		hourlyEmployee = new Boolean[numOfEmployees];
+		localResidents = new Boolean[numOfEmployees];
+		hourlyWorked = new float[numOfEmployees];
+		hourlyRate = new float[numOfEmployees];
+		salaryRate = new float[numOfEmployees];
+		grossIncome = new float[numOfEmployees];
+		netIncome = new float[numOfEmployees];
+		
 		// process employee payroll
-		ProcessEmployee(numOfEmployees, employeeNames, employeeIds, hourlyEmployee, hourlyWorked, hourlyRate, salaries, grossIncome, netIncome);
+		ProcessEmployee(employeeNames, employeeIds, hourlyEmployee, localResidents, hourlyWorked, hourlyRate, salaryRate, grossIncome, netIncome);
 		
 		// display payroll information
 		PayRollReport();
@@ -59,18 +71,18 @@ public class PROGRAM2
 		    }
 		    catch (Exception ex)
 		    {
-		    	System.out.println("INVLALID. Enter '1' or more.");
+		    	System.out.println("INVALID. Enter '1' or more.");
 		    }
 	    } while (numOfEmployees <= 0);
 	}
 	
-	public static void ProcessEmployee(String[] employeeNames, String[] employeeIds, Boolean[] hourlyEmployee, float[] hourlyWorked, float[] hourlyRate, float[] salaries, float[] grossIncome, float[] netIncome)
+	public static void ProcessEmployee(String[] employeeNames, String[] employeeIds, Boolean[] hourlyEmployee, Boolean[] localResidents, float[] hourlyWorked, float[] hourlyRate, float[] salaryRate, float[] grossIncome, float[] netIncome)
 	{
 		// fill employee data
 		for (int i = 0; i < numOfEmployees; i++)
 		{
 			// collect and validate employee id
-			employeeIds[i] = getEmployeeId(employeeIds);
+			employeeIds[i] = getEmployeeId(i, employeeIds);
 			
 			// collect and validate employee name
 			employeeNames[i] = getEmployeeName();
@@ -79,23 +91,26 @@ public class PROGRAM2
 			hourlyEmployee[i] = getEmployeeType();
 			
 			// collect and validate hourly hours worked
-			hourlyWorked[i] = getHourlyWorked();
+			hourlyWorked[i] = getHoursWorked();
 			
 			// collect and validate hourly rates
 			hourlyRate[i] = getHourlyRate();
 			
 			// collect and validate salary data
-			salaries[i] = getSalaries();
+			salaryRate[i] = getSalary();
 			
-			// collect and validate gross income
-			grossIncome[i] = getGrossIncome();
+			// collect and validate employee resident type
+			localResidents[i] = getEmployeeLocality();
+			
+			// calculate gross income
+			grossIncome[i] = getGrossIncome(hourlyEmployee[i], hourlyWorked[i], hourlyRate[i], salaryRate[i]);
 			
 			// collect and validate net income
-			netIncome[i] = getNetIncome();
+			netIncome[i] = getNetIncome(hourlyEmployee[i], hourlyWorked[i], hourlyRate[i], salaryRate[i]);
 		}
 	}
 	
-	public static String getEmployeeId(String[] employeeIds)
+	public static String getEmployeeId(int currentEmployee, String[] employeeIds)
 	{
 		// local temp var to validate user input
 		Boolean isValid = false;
@@ -105,7 +120,7 @@ public class PROGRAM2
 		do
 	    {
 			// display menu of methods
-			System.out.println("Enter data for employee (" + i + ")");
+			System.out.println("Enter data for employee (" + currentEmployee + ")");
 			System.out.println("\tEmployee ID:");
 			
 		    // check for valid numeric entry
@@ -134,6 +149,9 @@ public class PROGRAM2
 	        // valid id
 	        isValid = true;		        
 	    } while (!isValid);
+		
+		// return
+		return userInput;
 	}
 	
 	public static String getEmployeeName()
@@ -177,7 +195,7 @@ public class PROGRAM2
 		return (userInput.toLowerCase() == "h");
 	}
 	
-	public static float getHoursWorked()
+	public static Boolean getEmployeeLocality()
 	{
 		// local temp var to validate user input
 		Boolean isValid = false;
@@ -187,16 +205,16 @@ public class PROGRAM2
 		do
 	    {
 			// display menu of methods
-			System.out.println("\tHours worked this period:");
+			System.out.println("\tLocal resident (Y/N):");
 			
 		    // check for valid numeric entry
 	    	userInput = input.nextLine();
 	    	
 	    	// ensure ID is in correct format
-	        if (userInput.substring(0, 1).toLowerCase() != "h" && userInput.substring(0, 1).toLowerCase() != "s")
+	        if (userInput.toLowerCase() != "y" && userInput.toLowerCase() != "n")
 	        {
 	        	// notify user of duplicate id
-	    		System.out.println("This ID is invalid. Must start with 'H' or 'S'. Please try again.");
+	    		System.out.println("INVALID. Enter 'Y' or 'N'. Please try again.");
 	    		
 	    		// restart loop
 	    		continue;
@@ -205,5 +223,79 @@ public class PROGRAM2
 	        // valid id
 	        isValid = true;		        
 	    } while (!isValid);
+		
+		return (userInput.toLowerCase() == "y");
+	}
+	
+	public static float getHoursWorked()
+	{
+		// local temp var to hold user input
+		float hoursWorked = 0f;
+		
+		// display menu of methods
+		System.out.println("\tHours worked this period:");
+		
+	    // check for valid numeric entry
+    	hoursWorked = input.nextFloat();
+    	
+    	// consume carriage return
+    	input.nextLine();
+    	
+    	// return
+    	return hoursWorked;
+	}
+
+	public static float getHourlyRate()
+	{
+		// local temp var to hold user input
+		float hourlyRate = 0f;
+		
+		// display menu of methods
+		System.out.println("\tHourly Rate:");
+		
+	    // check for valid numeric entry
+		hourlyRate = input.nextFloat();
+    	
+    	// consume carriage return
+    	input.nextLine();
+    	
+    	// return
+    	return hourlyRate;
+	}
+	
+	public static float getSalary()
+	{
+		// local temp var to hold user input
+		float salary = 0f;
+		
+		// display menu of methods
+		System.out.println("\tWeekly salary:");
+		
+	    // check for valid numeric entry
+    	salary = input.nextFloat();
+    	
+    	// consume carriage return
+    	input.nextLine();
+    	
+    	// return
+    	return salary;
+	}
+	
+	public static float getGrossIncome(Boolean hourlyEmployee, float hoursWorked, float hoursRate, float salaryRate)
+	{
+		// calculate gross for hourly worker
+		if (hourlyEmployee)
+			return hoursWorked * hoursRate;
+		else
+			return salaryRate;
+	}
+	
+	public static float getNetIncome(Boolean hourlyEmployee, float hoursWorked, float hoursRate, float salaryRate)
+	{
+		// calculate gross for hourly worker
+		if (hourlyEmployee)
+			return hoursWorked * hoursRate;
+		else
+			return salaryRate;
 	}
 }
